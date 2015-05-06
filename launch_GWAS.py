4cdf4795128557514d304d0be4700aeb7ff1e06f
Ben Cipollini <bcipolli@ucsd.edu>
2015-05-06 10:15:52 -0700

    Add display vs. launch mode.

--- launch_GWAS.py
+++ launch_GWAS.py
@@ -1,6 +1,7 @@
 import datetime
 import json
-import requests
+import os
+import sys
 
 from ping import PINGSession
 
@@ -13,53 +14,89 @@ class GWASSession(PINGSession):
 
     def get_results_ids(self, force=False):
         assert self.sess is not None, "Session must be started before calling get_results_ids."
+        self.log("Retrieving all result IDs ...")
 
         if self.result_ids is None or force:
             resp = self.sess.get('https://ping-dataportal.ucsd.edu/applications/GWAS/getListOfRuns.php?project_name=PING')
             self.result_ids = json.loads(resp.text)['runs']
-            self.log('Fetched %d result ID(s)' % len(self.result_ids))
+            self.log("Fetched %d result ID(s)" % len(self.result_ids))
         return self.result_ids
 
-    def get_results(self, **kwargs):
+    def get_results(self, id=None, measure=None, raw=False, out_dir='download'):
+        """Can search by id or measure.
+        Search by id returns a list of tuples,
+            each tuple a triplet (SNP, effect size, pval)
+        Search by measure returns a list of list of tuples,
+            an outer list for each id corresponding to that measure.
+        """
+        assert int(id is None) + int(measure is None) == 1, 'id or measure (and not both) must be passed.'
         assert self.sess is not None, "Session must be started before calling get_results."
-        if 'measure' in kwargs:
-            ids = [r['id'] for r in self.get_results_ids()
-                           if r['yvalue'] == kwargs['measure']]
-            return [self.get_results(id=id) for id in ids]
-        url = 'https://ping-dataportal.ucsd.edu/applications/GWAS/downloadRunResult.php?project_name=PING&id=%s' % (kwargs['id'])
-        resp = self.sess.get(url)
-        if resp.text == u'':
-            raise Exception('id not found: %s' % kwargs['id'])
-        results = [lin.split('\t') for lin in resp.text.split('\n')]
 
-        self.log('Fetched %d result(s) for id=%s.' % (len(results), kwargs['id']))
-        return results
+        # Map measure to (possibly multiple) ids, then recursively call.
+        if measure is not None:
+            ids = [r['id'] for r in self.get_results_ids()
+                           if r['yvalue'] == measure]
+            self.log("Retrieving results for measure %s (%d ids found) ..." % (measure, len(ids)))
+            return [self.get_results(id=id, raw=raw, out_dir=out_dir) for id in ids]
+
+        # Fetch
+        out_file = os.path.join(out_dir, '%s_GWAS.csv' % id)
+        if os.path.exists(out_file):
+            with open(out_file, 'rb') as fp:
+                out_text = '\n'.join(fp.readlines())
+        else:
+            self.log("Retrieving results for id=%s ..." % id)
+            url = 'https://ping-dataportal.ucsd.edu/applications/GWAS/downloadRunResult.php?project_name=PING&id=%s' % id
+            resp = self.sess.get(url)
+            out_text = str(resp.text)
+            if out_text == '':
+                raise Exception('id not found: %s' % id)
+
+        # Cache the result
+        if not os.path.exists(out_file):
+            with open(out_file, 'wb') as fp:
+                fp.write(out_text)
+            self.log("Wrote results to disk at %s." % out_file)
+
+        # Parse the result
+        results = [lin.split('\t') for lin in out_text.split('\n')]
+        self.log("Fetched %d result(s) for id=%s." % (len(results), id))
+
+        return out_text if raw else results
 
     def launch_run(self, measure, covariates=['Age_At_IMGExam']):
         assert self.sess is not None, "Session must be started before calling launch_run."
         try:
-            self.log('Launching run for measure=%s ...' % measure)
+            self.log("Launching run for measure=%s ..." % measure)
             start_time = datetime.datetime.now()
             url = 'https://ping-dataportal.ucsd.edu/applications/GWAS/startRun.php?project_name=PING&com=%s&covariates=%s' % (
                 measure, '+'.join(covariates))
             print url
-            # resp = self.sess.get(url)
+            resp = self.sess.get(url)
             time_diff = datetime.datetime.now() - start_time
 
-            self.log('Completed run for measure=%s (time=%s)' % (measure, str(time_diff)))
-            self.get_result_ids(force=True)
+            self.log("Completed run for measure=%s (time=%s)" % (measure, str(time_diff)))
+            print "Results info:", self.get_results_ids(force=True)[-1]
         except Exception as e:
             print e
         else:
             print resp.text
 
-    def launch_and_retrieve_run(self, measure):
-        self.launch_run(measure=measure)
-        print self.get_results(measure=measure)
+    def launch_and_retrieve_run(self, measure, covariates=['Age_At_IMGExam'], out_dir='download'):
+        result_id = self.launch_run(measure=measure, covariates=covariates)
+        results = self.get_results(measure=measure, raw=True, out_dir=out_dir)
+        return results[-1]
 
 if __name__ == '__main__':
+    action = 'display' if len(sys.argv) == 1 else sys.argv[1]
+    measure = 'MRI_cort_area_ctx_transversetemporal_AI' if len(sys.argv) <= 2 else sys.argv[2]
+    covariates = ['MRI_cort_area_ctx_total_LH_PLUS_RH']
+    print action, measure, covariates
+
     sess = GWASSession()  # username/password stored in an env variable for me.
     sess.login()
-    print sess.get_results_ids()
-    print sess.get_results(measure=u'MRI_cort_area_ctx_frontalpole_AI')
-    # print sess.launch_and_retrieve_run(measure='MRI_cort_area_ctx_rostralanteriorcingulate_AI')
+
+    if action == 'display':
+        print sess.get_results(measure=measure)
+    elif action == 'launch':
+        print sess.launch_and_retrieve_run(measure=measure, covariates=covariates)
