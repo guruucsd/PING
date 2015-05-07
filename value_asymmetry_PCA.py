8305da523183a62c2834effbef682c968071dfdc
Ben Cipollini <bcipolli@ucsd.edu>
2015-05-07 10:55:27 -0700

    Improve PCA, associate with TCX and FDH measures.

--- value_asymmetry_PCA.py
+++ value_asymmetry_PCA.py
@@ -2,40 +2,17 @@
 """
 
 import numpy as np
+import scipy
 from scipy.stats import pearsonr
 from sklearn.decomposition import PCA
 
 from ping import col2prop, get_asymmetry_index, load_PING_data, get_twohemi_keys
+from similarity_matrices import get_all_data
 from utils import do_and_plot_regression
 
 
-def show_asymmetry_by_component(prefix, grouping_prop_name='FDH_23_Handedness_Prtcpnt', whiten=True, asymmetry_threshhold=0.05):
-    """ Grab all data matching prefix, split into groups by grouping_prop_name,
-    then perform PCA and show the PCs."""
-
-    data = load_PING_data()
-    good_keys = get_twohemi_keys(prefix, data.keys())
-
-    # Narrow to selected keys
-    data_subset = []
-    for key in good_keys:
-        data_subset.append(get_asymmetry_index(data, key))
-    data_subset = np.asarray(data_subset)
-
-    # Eliminate subjects with zero asymmetry in anything (likely artifacts)
-    noasymm_idx = np.abs(data_subset).sum(axis=0) != 0.
-    print("Removing %d subjects with ZERO asymmetry "
-          "(over %d measures!)" % (int(np.logical_not(noasymm_idx).sum()),
-                                   len(good_keys)))
-    data_subset = data_subset[:, noasymm_idx]
-
-    # Narrow to selected keys with mean(asymmetry_index) > 0.05
-    mean_asymmetry = data_subset.mean(axis=1)
-    asymmetric_idx = np.abs(mean_asymmetry) >= asymmetry_threshhold
-    data_subset = data_subset[asymmetric_idx]
-    good_keys = good_keys[asymmetric_idx]
-    mean_asymmetry = mean_asymmetry[asymmetric_idx]
-
+def report_asymmetries():
+    """Stale code moved from elsewhere."""
     # Report a summary of the asymmetries
     n_measures = len(good_keys)
     all_thresh = [1.0, 0.25, 0.10, 0.05, 0.01]
@@ -51,33 +28,95 @@ def show_asymmetry_by_component(prefix, grouping_prop_name='FDH_23_Handedness_Pr
             for idx in np.nonzero(hemi_idx.astype(int))[0]:
                 print("\t\t%s (%.2f)" % (good_keys[idx], mean_asymmetry[idx]))
 
+
+def show_asymmetry_by_component(prefix, grouping_prop_name='FDH_23_Handedness_Prtcpnt',
+                                whiten=True, pc_threshhold=0.025):
+    """ Grab all data matching prefix, split into groups by grouping_prop_name,
+    then perform PCA and show the PCs."""
+
+    data = get_all_data(prefix=prefix + ['TBX_', 'FDH_'])
+    good_keys = get_twohemi_keys(prefix, data.keys())
+    good_keys = np.asarray([key for key in good_keys if 'fuzzy' not in key])
+
+    # Narrow to selected keys
+    data_subset = []
+    for key in good_keys:
+        data_subset.append(get_asymmetry_index(data, key)) #/ data['MRI_cort_area_ctx_total_LH_PLUS_RH'])
+    data_subset = np.asarray(data_subset)
+
+    # Eliminate subjects with zero asymmetry in anything (likely artifacts)
+    nan_idx = np.isnan(data_subset.sum(axis=0))
+    asymm_mag = np.abs(data_subset).sum(axis=0)
+    noasymm_idx = asymm_mag < asymm_mag.max() / 1000.
+    good_subj_idx = np.logical_not(np.logical_or(nan_idx, noasymm_idx))
+    data_subset = data_subset[:, good_subj_idx]
+    print "Eliminated %d subjects with zero asymmetry, %d with a nan measure." % (
+        sum(noasymm_idx), sum(nan_idx))
+    # if whiten:
+    # data_subset = scipy.stats.mstats.zscore(data_subset, axis=1)
+
+    # Narrow to selected keys with mean(asymmetry_index) > 0.05
+    mean_asymmetry = data_subset.std(axis=1)
+    # asymmetric_idx = np.abs(mean_asymmetry) >= asymmetry_threshhold
+    # data_subset = data_subset[asymmetric_idx]
+    # good_keys = good_keys[asymmetric_idx]
+    # mean_asymmetry = mean_asymmetry[asymmetric_idx]
+
     # Compute PCA and dump the results
     pca = PCA(whiten=whiten)
     pca.fit(data_subset.T)
-    selected_idx = pca.explained_variance_ratio_ >= 0.05
+    selected_idx = pca.explained_variance_ratio_ >= pc_threshhold
     for pi, pc in enumerate(pca.components_[selected_idx]):
         print "%2d: (%.2f):" % (pi, pca.explained_variance_ratio_[pi])
-        for hemi_sign, hemi_name in zip([1, -1], ['lh', 'rh']):
-            for key, coeff, mag in zip(good_keys, pc, mean_asymmetry):
-                if np.sign(mag) == hemi_sign:
-                    print "\t%s %s%.4f %s (%.2f)" % (hemi_name, ' ' if coeff >= 0 else '', coeff, key, 1000*mag*coeff)
+        sort_idx = np.argsort(np.abs(pc) * mean_asymmetry)
+        for key, coeff, mag in zip(good_keys[sort_idx], pc[sort_idx], mean_asymmetry[sort_idx]):
+            # if np.sign(mag) == hemi_sign:
+            print "\t%s%.4f %-50s (%.3e / %.3e)" % (
+                ' ' if mag*coeff >= 0 else '',
+                mag*coeff,
+                key,
+                coeff,
+                mag)
 
     # Now, take this factor and compare it to behavioral data
     # (or, should I throw the behavior in the PCA?)
-    tbx_props = filter(lambda k: 'TBX_' in k, data.keys())
-    tbx_data = np.asarray(data[tbx_props].to_records())[noasymm_idx]
+    tbx_data = dict()
+    for key in data.keys():
+        if not key.startswith('TBX_'):
+            continue
+        if data[key].dtype.name in ['string', 'object']:
+            continue
+        tbx_data[key] = data[key][good_subj_idx]
+    print 'Found %d TBX keys.' % len(tbx_data)
+
     for pi, pc in enumerate(pca.components_[selected_idx]):
         pc_projection = np.dot(pc, data_subset)
-        for ti, tbx_prop in enumerate(tbx_props):
-            row_data = np.asarray([r[ti] for r in tbx_data])
-            if 'string' in row_data.dtype.name:
-                continue
-            good_subj = np.logical_not(np.isnan(row_data))
-            rval, pval = pearsonr(pc_projection[good_subj], row_data[good_subj])
+        for ti, (key, val) in enumerate(tbx_data.items()):
+            cur_idx = np.logical_not(np.isnan(val))
+            rval, pval = pearsonr(pc_projection[cur_idx], val[cur_idx])
             if pval < 0.05:
-                print('%d: %s %.4f, %.4f' % (pi, tbx_prop, rval, pval))
+                print('%d: %-20s (n=%4d) r=%.4f, p=%.4f' % (pi, key, cur_idx.sum(), rval, pval))
+
+
+    # Now, take this factor and compare it to hisory data
+    # (or, should I throw the behavior in the PCA?)
+    fdh_data = dict()
+    for key in data.keys():
+        if not key.startswith('FDH_'):
+            continue
+        if data[key].dtype.name in ['string', 'object']:
+            continue
+        fdh_data[key] = data[key][good_subj_idx]
+    print 'Found %d FDH keys.' % len(fdh_data)
+
+    for pi, pc in enumerate(pca.components_[selected_idx]):
+        pc_projection = np.dot(pc, data_subset)
+        for ti, (key, val) in enumerate(fdh_data.items()):
+            cur_idx = np.logical_not(np.isnan(val))
+            rval, pval = pearsonr(pc_projection[cur_idx], val[cur_idx])
+            if pval < 0.05:
+                print('%d: %-20s (n=%4d) r=%.4f, p=%.4f' % (pi, key, cur_idx.sum(), rval, pval))
 
 
 if __name__ == '__main__':
-    show_asymmetry_by_component(prefix=['MRI_subcort_vol', 'MRI_cort_thick',
-                                        'MRI_cort_area', 'DTI_fiber_vol'])
+    show_asymmetry_by_component(prefix=['MRI_cort_area'])
