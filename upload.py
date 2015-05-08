f0dbe61dd7f7fb62e8f809ad47bb19dbe8bb687a
Ben Cipollini <bcipolli@ucsd.edu>
2015-05-08 04:08:08 -0700

    Create script to upload spreadsheets to the server.

--- upload.py
+++ upload.py
@@ -1,9 +1,19 @@
 """
 """
-from data import EXPORTED_PING_SPREADSHEET
+from data import get_derived_data, EXPORTED_PING_SPREADSHEET
 from ping.apps.upload import PINGUploadSession
+from ping.export import export_all_data
 
-if __name__ == '__main__':
-    sess = PINGUploadSession()
-    sess.login()
-    sess.upload_spreadsheet(EXPORTED_PING_SPREADSHEET)
+
+# Export the relevant data
+prefixes = ['MRI_cort_area', 'MRI_cort_thick', 'MRI_subcort_vol', 'DTI_fiber_vol']
+export_data = get_derived_data(prefix=prefixes, force=True)
+export_all_data(export_data, out_file=EXPORTED_PING_SPREADSHEET)
+
+for key in sorted(export_data.keys()):
+    print key
+
+# Upload the new spreadsheet.
+#sess = PINGUploadSession()
+#sess.login()
+#sess.upload_spreadsheet(EXPORTED_PING_SPREADSHEET)
