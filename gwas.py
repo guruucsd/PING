c25938931724c95c2d6d474fb9ea5de8a1dd64e5
bcipolli <bcipolli@ucsd.edu>
2015-05-11 10:07:53 -0700

    Add 'usage' for gwas command.

--- gwas.py
+++ gwas.py
@@ -6,15 +6,38 @@ import sys
 from ping.apps.gwas import GWASSession
 
 
-action = 'display' if len(sys.argv) == 1 else sys.argv[1]
-measure = 'MRI_cort_area_PC0' if len(sys.argv) <= 2 else sys.argv[2]
-covariates = ['Age_At_IMGExam']  # ', 'DTI_fiber_vol_AllFibnoCC_AI', 'Gender', 'FDH_23_Handedness_Prtcpnt']  # MRI_cort_area_ctx_total_LH_PLUS_RH']
-print(action, measure, covariates)
+def do_usage(args):
+    print("\nUsage for %s:" % args[0])
+    print("\t%s {action} {measure}" % args[0])
+    print("\n\taction: 'display' or 'launch'")
+    print("\t\tdisplay: show results from a previous run")
+    print("\t\tlaunch: launch a new GWAS run")
+    print("\tmeasure: any measure from the PING database, or measure uploaded via upload.py")
+    print("\t\tThe measure will be regressed against variation in genes at each SNP.")
+    print("\t\tAge_At_ImgExam will be used as the covariate.")
+    print("\nExamples:")
+    print("\t%s launch MRI_cort_area_ctx_total_LH_PLUS_RH" % args[0])
+    print("\t\tThis launches a GWAS to search for genetic variation as a function of total cortical area.")
+    print("\t%s display MRI_cort_area_ctx_total_LH_PLUS_RH" % args[0])
+    print("\t\tThis downloads and displays the top 200 SNPs related to total cortical area.")
 
-sess = GWASSession()  # username/password stored in an env variable for me.
-sess.login()
 
-if action == 'display':
-    print(sess.get_results(measure=measure, force=True))
-elif action == 'launch':
-    print(sess.launch_and_retrieve_run(measure=measure, covariates=covariates))
+if __name__ != '__main__':
+    pass
+
+elif len(sys.argv) != 3:
+    do_usage(sys.argv)
+
+else:
+    action = sys.argv[1]
+    measure = sys.argv[2]
+    covariates = ['Age_At_IMGExam']  # ', 'DTI_fiber_vol_AllFibnoCC_AI', 'Gender', 'FDH_23_Handedness_Prtcpnt']  # MRI_cort_area_ctx_total_LH_PLUS_RH']
+    print(action, measure, covariates)
+
+    sess = GWASSession()  # username/password stored in an env variable for me.
+    sess.login()
+
+    if action == 'display':
+        print(sess.get_results(measure=measure, force=True))
+    elif action == 'launch':
+        print(sess.launch_and_retrieve_run(measure=measure, covariates=covariates))
