58fff589f643b4c24d65258c8da15185941caa50
Ben Cipollini <bcipolli@ucsd.edu>
2015-05-12 20:59:57 -0700

    Make labels on similarity matrix plot more meaningful.

--- ping/utils/plotting.py
+++ ping/utils/plotting.py
@@ -15,21 +15,47 @@ def plot_symmetric_matrix_as_triangle(mat, ax=None, lbls=None, vmin=0, vmax=1):
     if len(mat.shape) == 1:
         # Convert vector to matrix
         mat = scipy.spatial.distance.squareform(mat)
+    mat[np.eye(mat.shape[0], dtype=bool)] = 0
 
     # Mask the matrix; will lead to transparency in imshow
     masked_mat = np.ma.masked_where(np.triu(np.eye(mat.shape[0])), mat)
 
     # interpolation: none needed for transparency...
     ax.set_axis_bgcolor(ax.get_figure().get_facecolor())
-    ax.imshow(mat, vmin=vmin, vmax=vmax, interpolation='none')
+    img = ax.imshow(mat, vmin=vmin, vmax=vmax, interpolation='nearest')
     ax.set_frame_on(False)
+    plt.colorbar(img)
 
-    if lbls:
-        sz = mat.shape[0]
-        ax.set_xticks(range(sz - 1))
-        ax.set_xticklabels(lbls[:sz - 1])
-        ax.set_yticks(range(1, sz))
-        ax.set_yticklabels(lbls[1:])
+    lbl_class = lambda lbl: lbl.split('.')[0]
+    if not lbls:
+        ax.set_xticks([])
+        ax.set_yticks([])
+    else:
+        lbl_classes = np.unique([lbl_class(lbl) for lbl in lbls])
+        if len(lbl_classes) == 1:
+            sz = mat.shape[0]
+            ax.set_xticks(range(sz - 1))
+            ax.set_xticklabels(lbls[:sz - 1])
+            ax.set_yticks(range(1, sz))
+            ax.set_yticklabels(lbls[1:])
+        else:
+            border_idx = [0]
+            border_lbls = [lbl_class(lbls[0])]
+            for li, lbl in enumerate(lbls[1:]):
+                if lbl_class(lbl) != border_lbls[-1]:
+                    print('Adding %s after %s' % (lbl_class(lbl),
+                                                  border_lbls[-1]))
+                    # Add the previous again at the border, to
+                    #   clarify the boundary.
+                    # border_idx.append(li - 1)
+                    # border_lbls.append(border_lbls[-1])
+                    # Now add the new one
+                    border_idx.append(li)
+                    border_lbls.append(lbl_class(lbl))
+            ax.set_yticks(border_idx)
+            ax.set_yticklabels(border_lbls)
+            ax.set_xticks([])
+
 
 
 def equalize_xlims(fh):
