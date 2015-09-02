0f1d0f2efd95f75d8f83971138a267eec9958a7a
Ben Cipollini <bcipolli@ucsd.edu>
2015-09-02 06:48:07 -0700

    Turn similarity script into function, allow end-to-end testing.

---
+++ tests.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+
+from similarity import do_similarity
+from snps import do_snps
+
+
+class TestWithNonblockingPlots(object):
+    def setUp(self):
+        plt.ion()
+
+
+class TestSimilarity(TestWithNonblockingPlots):
+    def test_similarity(self):
+        do_similarity('MRI_cort_area.ctx', 'partial-correlation', 'Left Hemisphere')
