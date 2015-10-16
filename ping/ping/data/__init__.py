618ada55bb4d9fb3b15480fa85967a992e4f1fba
Ben Cipollini <bcipolli@ucsd.edu>
2015-10-16 14:51:13 -0500

    Add prefix/atlas associations.

--- ping/ping/data/__init__.py
+++ ping/ping/data/__init__.py
@@ -1,3 +1,6 @@
 from .base import filter_data, merge_by_key
 from .default import PINGData
 from .destrieux import DestrieuxData
+
+prefixes = dict(desikan=dict(area='MRI_cort_area.ctx.', thickness='MRI_cort_thick.ctx.'),
+                destrieux=dict(area='Destrieux_area.', thickness='Destrieux_thickness.'))
