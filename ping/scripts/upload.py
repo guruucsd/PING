5e543c44135a8dfb55df6842f075d0a241b80065
Ben Cipollini <bcipolli@ucsd.edu>
2015-11-01 21:00:36 -0800

    Fix up upload.

--- ping/scripts/upload.py
+++ ping/scripts/upload.py
@@ -10,21 +10,16 @@ from ..ping.apps import PINGSession
 from ..research.data import get_all_data
 
 
-def do_upload(*args):
-    raise NotImplementedError('Broken in refactor.')
-    if len(args) > 0:
-        # Someone told us about an existing csv
-        csv_file = args[0]
+def do_upload(csv_file=EXPORTED_PING_SPREADSHEET,
+              atlas='desikan', data_dir='data',
+              username=None, passwd=None, force=False):
 
-    else:
-        # We will build a csv on the fly.
-        csv_file = EXPORTED_PING_SPREADSHEET
-
+    if force or not os.path.exists(csv_file):
         # Limit to imaging data only
-        data = get_all_data()
-        data.filter(filter_fns=[partial(lambda k, v, p: k.startswith(p), p=p)
-                                for p in data.IMAGING_PREFIX])
-
+        data = get_all_data(atlas, data_dir=data_dir,
+                            username=username, passwd=passwd)
+        # data.filter(filter_fns=[partial(lambda k, v, p: k.startswith(p), p=p)
+        #                         for p in data.IMAGING_PREFIX])
         data.export(out_file=csv_file)
 
     # Upload the new spreadsheet.
@@ -32,6 +27,12 @@ def do_upload(*args):
     sess.login()
     sess.upload_user_spreadsheet(csv_file)
 
+
 if __name__ == '__main__':
-    import sys
-    do_upload(*sys.argv[1:])
+    parser = ResearchArgParser(description="Upload data spreadsheet to the PING portal.",
+                               common_args=['atlas', 'data-dir',
+                                            'username', 'passwd', 'force'])
+    parser.add_argument('--csv_file', nargs='?', default=EXPORTED_PING_SPREADSHEET)
+
+    args = parser.parse_args()
+    do_upload(**vars(args))
